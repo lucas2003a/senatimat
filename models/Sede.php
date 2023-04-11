1cb12fff42db5e30ccfc760d62a0c9d95ad8bc89
Jhon <jfranciaminaya@gmail.com>
2023-04-11 14:50:35 -0500

    Metodos listar sede, escuela y carrera terminado

---
+++ models/Sede.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'Conexion.php';
+
+class Sede extends Conexion{
+
+  private $accesoBD;
+
+  public function __CONSTRUCT(){
+    $this->accesoBD = parent::getConexion();
+  }
+
+  public function listarSedes(){
+    try{
+      $consulta = $this->accesoBD->prepare("CALL spu_sedes_listar()");
+      $consulta->execute();
+      return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e){
+      die($e->getMessage());
+    }
+  }
+
+}
