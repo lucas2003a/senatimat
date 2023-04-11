2c09bc699e2c54ef4b2fc004c021ad380d722334
Jhon <jfranciaminaya@gmail.com>
2023-04-11 14:33:30 -0500

    Inicio de proyecto

---
+++ database/Base de datos.sql
@@ -0,0 +1,85 @@
+CREATE DATABASE senatimat;
+USE senatimat;
+
+CREATE TABLE escuelas
+(
+	idescuela 		INT AUTO_INCREMENT PRIMARY KEY,
+	escuela 			VARCHAR(50) 	NOT NULL,
+	CONSTRAINT uk_escuela_esc UNIQUE (escuela)
+)ENGINE = INNODB;
+
+INSERT INTO escuelas (escuela) VALUES
+	('ETI'), -- 1
+	('Administración'), -- 2
+	('Metal mecánica'); -- 3
+
+SELECT * FROM escuelas ORDER BY 1;
+
+-- ***** SEGUNDA TABLA ***** --
+
+CREATE TABLE carreras
+(
+	idcarrera 		INT AUTO_INCREMENT PRIMARY KEY,
+	idescuela 		INT 			NOT NULL,
+	carrera 			VARCHAR(100)NOT NULL,
+	CONSTRAINT fk_idescuela_car FOREIGN KEY (idescuela) REFERENCES escuelas (idescuela),
+	CONSTRAINT uk_carrera_car UNIQUE (carrera)
+)ENGINE = INNODB;
+
+INSERT INTO carreras (idescuela, carrera) VALUES
+	(1, 'Diseño Gráfico Digital'),
+	(1, 'Ingeniería de Software con IA'),
+	(1, 'Cyberseguridad'),
+	(2, 'Administración de empresas'),
+	(2, 'Administración Industrial'),
+	(2, 'Prevencionista de Riesgo'),
+	(3, 'Soldador Universal'),
+	(3, 'Mecánico de mantenimiento'),
+	(3, 'Soldador estructuras metálicas');
+
+SELECT * FROM carreras ORDER BY 1;
+
+-- ****** TERCERA TABLA ******* --
+CREATE TABLE sedes
+(
+	idsede 		INT AUTO_INCREMENT PRIMARY KEY,
+	sede 			VARCHAR(40)		NOT NULL,
+	CONSTRAINT uk_sede_sde UNIQUE (sede)
+)ENGINE = INNODB;
+
+INSERT INTO sedes (sede) VALUES
+	('Chincha'),
+	('Pisco'),
+	('Ica'),
+	('Ayacucho');
+
+SELECT * FROM sedes ORDER BY 1;
+
+-- ******* CUARTA TABLA ********* --
+CREATE TABLE estudiantes
+(
+	idestudiante 		INT AUTO_INCREMENT PRIMARY KEY,
+	apellidos			VARCHAR(40)		NOT NULL,
+	nombres 				VARCHAR(40)		NOT NULL,
+	tipodocumento 		CHAR(1)			NOT NULL DEFAULT 'D',
+	nrodocumento		CHAR(8)			NOT NULL,
+	fechanacimiento	DATE 				NOT NULL,
+	idcarrera 			INT 				NOT NULL,
+	idsede 				INT 				NOT NULL,
+	fotografia 			VARCHAR(100)	NULL,
+	fecharegistro		DATETIME 		NOT NULL DEFAULT NOW(),
+	fechaupdate 		DATETIME 		NULL,
+	estado 				CHAR(1)			NOT NULL DEFAULT '1',
+	CONSTRAINT uk_nrodocumento_est UNIQUE (tipodocumento, nrodocumento),
+	CONSTRAINT fk_idcarrera_est FOREIGN KEY (idcarrera) REFERENCES carreras (idcarrera),
+	CONSTRAINT fk_idsede_est FOREIGN KEY (idsede) REFERENCES sedes (idsede)
+)ENGINE = INNODB;
+
+INSERT INTO estudiantes 
+	(apellidos, nombres, nrodocumento, fechanacimiento, idcarrera, idsede) VALUES
+	('Martinez', 'Carlos', '44445555', '2000-01-01', 1, 1),
+	('Ochoa', 'Fiorella', '77778888', '2000-10-10', 4, 2),
+	('Perez', 'Roxana', '88881111', '2001-03-03', 7, 3),
+	('Quintana', 'Tania', '33334444', '2001-05-05', 9, 4);
+
+SELECT * FROM estudiantes;
---
+++ database/Procedimientos almacenados.sql
@@ -0,0 +1,71 @@
+USE senatimat;
+
+DELIMITER $$
+CREATE PROCEDURE spu_estudiantes_listar()
+BEGIN
+	SELECT	EST.idestudiante,
+				EST.apellidos, EST.nombres,
+				EST.tipodocumento, EST.nrodocumento,
+				EST.fechanacimiento,
+				ESC.escuela,
+				CAR.carrera,
+				SED.sede,
+				EST.fotografia
+		FROM estudiantes EST
+		INNER JOIN carreras CAR ON CAR.idcarrera = EST.idcarrera
+		INNER JOIN sedes SED ON SED.idsede = EST.idsede
+		INNER JOIN escuelas ESC ON ESC.idescuela = CAR.idescuela
+		WHERE EST.estado = '1';
+END $$
+
+
+DELIMITER $$
+CREATE PROCEDURE spu_estudiantes_registrar
+(
+	IN _apellidos 			VARCHAR(40),
+	IN _nombres 			VARCHAR(40),
+	IN _tipodocumento		CHAR(1),
+	IN _nrodocumento		CHAR(8),
+	IN _fechanacimiento	DATE,
+	IN _idcarrera 			INT,
+	IN _idsede 				INT,
+	IN _fotografia 		VARCHAR(100)
+)
+BEGIN
+	-- Validar el contenido de _fotografia
+	IF _fotografia = '' THEN 
+		SET _fotografia = NULL;
+	END IF;
+
+	INSERT INTO estudiantes 
+	(apellidos, nombres, tipodocumento, nrodocumento, fechanacimiento, idcarrera, idsede, fotografia) VALUES
+	(_apellidos, _nombres, _tipodocumento, _nrodocumento, _fechanacimiento, _idcarrera, _idsede, _fotografia);
+END $$
+
+CALL spu_estudiantes_registrar('Francia Minaya', 'Jhon', 'D', '12345678', '1984-09-20', 5, 1, '');
+CALL spu_estudiantes_registrar('Munayco', 'José', 'D', '77779999', '1999-09-20', 3, 2, NULL);
+CALL spu_estudiantes_registrar('Prada', 'Teresa', 'C', '01234567', '2002-09-25', 3, 2, '');
+SELECT * FROM estudiantes;
+
+DELIMITER $$
+CREATE PROCEDURE spu_sedes_listar()
+BEGIN
+	SELECT * FROM sedes ORDER BY 2;
+END $$
+
+DELIMITER $$
+CREATE PROCEDURE spu_escuelas_listar()
+BEGIN 
+	SELECT * FROM escuelas ORDER BY 2;
+END $$
+
+DELIMITER $$
+CREATE PROCEDURE spu_carreras_listar(IN _idescuela INT)
+BEGIN
+	SELECT idcarrera, carrera 
+		FROM carreras
+		WHERE idescuela = _idescuela;
+END $$
+
+CALL spu_carreras_listar(3);
+
